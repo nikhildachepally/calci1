431b17f42425e5558fb1e3373d1b64d397507c75
nikhildachepally <nikhil.dachepally@gmail.com>
2016-01-29 23:32:22 +0530

    commit

---
+++ logic.js
@@ -0,0 +1,20 @@
+var box=document.getElementById('display');
+
+function addtoscreen(x)
+{
+	box.value+=x;
+
+	if(x=='c')
+	{
+
+		box.value='';
+	}
+}
+ 
+ function answer()
+
+ {
+ 	x=box.value;
+ 	x=eval(x);
+ 	box.value= x;
+ }
